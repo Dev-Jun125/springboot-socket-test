3f5f631b9001408d5fdc6887b93926e703aa1935
Dev-Jun125 <dev.jun125@gmail.com>
2023-01-13 14:24:07 +0900

    first commit

---
+++ src/main/java/com/example/demo/test/MainRestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api")
+public class MainRestController {
+    @Autowired
+    WebSocket webSocket;
+
+    @GetMapping(value = "/customer_len")
+    public ResponseEntity<?> main() {
+        String msg = "" + webSocket.getSessionList();
+        System.out.println("api");
+        return new ResponseEntity<String>(msg, HttpStatus.OK);
+    }
+
+}
